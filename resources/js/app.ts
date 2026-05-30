1e529306a3fed808726b3876bf64ac48d0a73018
janthetrashcan <janerikolim@gmail.com>
2026-05-30 22:30:33 +0800

    feat: primevue init
    Removed template Vue pages

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,33 +1,14 @@
 import { createInertiaApp } from '@inertiajs/vue3';
-import { initializeTheme } from '@/composables/useAppearance';
-import AppLayout from '@/layouts/AppLayout.vue';
-import AuthLayout from '@/layouts/AuthLayout.vue';
-import SettingsLayout from '@/layouts/settings/Layout.vue';
-import { initializeFlashToast } from '@/lib/flashToast';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    layout: (name) => {
-        switch (true) {
-            case name === 'Welcome':
-                return null;
-            case name.startsWith('auth/'):
-                return AuthLayout;
-            case name.startsWith('settings/'):
-                return [AppLayout, SettingsLayout];
-            default:
-                return AppLayout;
-        }
-    },
-    progress: {
-        color: '#4B5563',
+    withApp(app) {
+        app.use(PrimeVue, {
+            theme: {
+                preset: Aura,
+            },
+            ripple: true,
+        });
     },
 });
-
-// This will set light / dark mode on page load...
-initializeTheme();
-
-// This will listen for flash toast data from the server...
-initializeFlashToast();
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
+
+Route::get('/test', function() {
+    return Inertia::render('Test/Hello');
+});
+
 
 require __DIR__.'/settings.php';
