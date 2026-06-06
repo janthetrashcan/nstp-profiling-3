a2de0cd0dc87940ae7daab935b6ec82b97d9fbb7
Jan <janerikolim@gmail.com>
2026-06-07 03:03:08 +0800

    flowbite config

--- resources/js/pages/Test.vue
+++ resources/js/pages/Test.vue
@@ -1,5 +1,10 @@
 <template>
-    <p>
-        Hello world
-    </p>
+    <p class="font-bold">Hello world</p>
+
+    <button
+        type="button"
+        class="box-border rounded-base border border-transparent bg-brand px-4 py-2.5 text-sm leading-5 font-medium text-white shadow-xs hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium focus:outline-none"
+    >
+        Default
+    </button>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,5 +12,7 @@
 </head>
 <body class="font-sans antialiased">
     @inertia
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
 </body>
 </html>
