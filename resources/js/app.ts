b93ecc6c86aad2a263396ab40f628496e1ba37e0
janthetrashcan <janerikolim@gmail.com>
2026-08-13 21:34:20 +0800

    removed old vue and postcss dependencies

--- resources/js/app.ts
+++
@@ -1,7 +0,0 @@
-import '../css/app.css';
-import { createInertiaApp } from '@inertiajs/vue3';
-import DashboardLayout from './layouts/Dashboard/DashboardLayout.vue';
-
-createInertiaApp({
-    layout: () => DashboardLayout,
-});
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.js"])
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    @inertiaHead
-</head>
-<body class="font-sans antialiased">
-    @inertia
-
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
-</body>
+        <!-- Scripts -->
+        @routes
+        @viteReactRefresh
+        @vite('resources/css/app.css')
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
